85a65dc41d80abc7bc1dd7556544610c72d0c3f4
Ceyda Esen <ceydae317@gmail.com>
2023-08-11 17:32:37 +0430

    chore: add simple functionality for streams and config Cargo.toml file"

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+use soroban_sdk::{contract, contractimpl, Address, Env};
+
+pub trait FluxityTrait {
+    fn init(e: Env, admin: Address);
+    fn stream(e: Env);
+    fn create_stream(e: Env);
+    fn cancel_stream(e: Env);
+    fn withdraw_stream(e: Env);
+    fn top_up_stream(e: Env);
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[contract]
+pub struct Fluxity;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[contractimpl]
+impl FluxityTrait for Fluxity {
+    fn init(_e: Env, _admin: Address) {}
+    fn stream(_e: Env) {}
+    fn create_stream(_e: Env) {}
+    fn cancel_stream(_e: Env) {}
+    fn withdraw_stream(_e: Env) {}
+    fn top_up_stream(_e: Env) {}
 }
+
+// #[cfg(test)]
+// mod tests;
